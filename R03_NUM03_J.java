59f2c43d451a38e67d10ecafa949a81b4dd96332
Andy Garcia <60903978+AndyGarcia102@users.noreply.github.com>
2022-10-15 15:41:10 -0400

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,9 +1,3 @@
-public static int getInteger(DataInputStream is ) throws IOException {
-	return is.readInt();
-}
-
-//Correct the code as shown in theCompliant Solution below:
-
 public static long getInteger (DataInputStream	is ) throws IOException {
 	return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one−bits
 }
