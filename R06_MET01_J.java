f18b8da17ad066afa6147e354979c2bcb39bba1e
Andy Garcia <60903978+AndyGarcia102@users.noreply.github.com>
2022-10-15 15:41:44 -0400

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,14 +1,3 @@
-public static int getAbsAdd ( int x , int y ) {
-    assert x != Integer.MINVALUE;
-    assert y != Integer.MINVALUE;
-    int absX = Math.abs(x);
-    int absY = Math.abs(y);
-    assert(absX <= Integer.MAXVALUE - absY);
-    return absX + absY;
-}
-//Usage: getAbsAdd(Integer.MINVALUE,1);
-//Correct the code as shown in the Compliant Solution below:
-
 public static int getAbsAdd (int x ,int y){
     if(x == Integer.MINVALUE || y == Integer.MINVALUE){
         thrownewIllegalArgumentException();
